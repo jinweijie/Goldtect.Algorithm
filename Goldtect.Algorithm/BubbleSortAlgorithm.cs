26c23c2386cc3b5af203deef360ff5abaa6f1f46
Weijie JIN <jinweijie@gmail.com>
2014-05-11 22:35:56 +0800

    initial

---
+++ Goldtect.Algorithm/BubbleSortAlgorithm.cs
@@ -0,0 +1,22 @@
+﻿namespace Goldtect.Algorithm
+{
+    class BubbleSortAlgorithm
+    {
+        public static void Sort(ref int[] arr)
+        {
+            int temp;
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int j = 0; j <= i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Goldtect.Algorithm/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goldtect.Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestFibonacci();
+            TestBubbleSort();
+        }
+
+        public static void TestFibonacci()
+        {
+            Console.WriteLine("TestFibonacci:");
+
+            for (int i = 0; i < 20; i++)
+                Console.Write(string.Format("{0} ",FibonacciAlgorithm.Fibonacci(i)));
+
+            Console.WriteLine();
+        }
+
+        public static void TestBubbleSort()
+        {
+            Console.WriteLine("TestBubbleSort:");
+
+            int[] arr = new int[] { 99, 88, 88, 66, 102, -22};
+
+            BubbleSortAlgorithm.Sort(ref arr);
+
+            foreach (var i in arr)
+                Console.Write(string.Format("{0} ", i));
+
+            Console.WriteLine();
+        }
+    }
+}
