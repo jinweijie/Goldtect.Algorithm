c6973e98c05d076cc721c3305034f645a5532963
Weijie JIN <jinweijie@gmail.com>
2014-05-11 22:49:33 +0800

    add singleton

---
+++ Goldtect.Algorithm/Singleton.cs
@@ -0,0 +1,29 @@
+﻿namespace Goldtect.Algorithm
+{
+    sealed class Singleton
+    {
+        private static volatile Singleton _instance;
+        private static object _syncRoot = new object();
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Singleton();
+                        }
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
